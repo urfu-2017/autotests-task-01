1d0e35e3bd728138e236ee43c607a1a5c4808b0f
graf-m <graf.m.96@yandex.ru>
2018-04-09 23:48:10 +0500

    first attempt

--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -2,11 +2,73 @@ const assert = require('assert');
 const getPokerHand = require('../lib/getPokerHand');
 
 describe('getPokerHand', () => {
-    it('should return `Покер` for [1, 1, 1, 1, 1]', () => {
-        const actual = getPokerHand([1, 1, 1, 1, 1]);
+    describe('Correct input', () => {
+        const nameToCombinations = {
+            Покер: [
+                [1, 1, 1, 1, 1],
+                [5, 5, 5, 5, 5]
+            ],
+            Каре: [
+                [1, 1, 2, 1, 1],
+                [1, 5, 5, 5, 5]
+            ],
+            'Фулл хаус': [
+                [2, 2, 2, 1, 1],
+                [6, 3, 6, 3, 6]
+            ],
+            Тройка: [
+                [1, 2, 6, 6, 6],
+                [5, 2, 5, 1, 5],
+            ],
+            'Две пары': [
+                [5, 6, 2, 2, 5],
+                [2, 1, 3, 2, 3],
+            ],
+            Пара: [
+                [6, 5, 4, 4, 3],
+                [2, 3, 6, 3, 1],
+            ],
+            'Наивысшее очко': [
+                [1, 2, 3, 4, 5],
+                [6, 2, 4, 1, 5]
+            ]
+        };
 
-        assert.equal(actual, 'Покер');
+        for (let combinationName in nameToCombinations) {
+            describe(`${combinationName}`, () => {
+                for (let combination of nameToCombinations[combinationName]) {
+                    it(`should return \`${combinationName}\` for [${combination}]`, () => {
+                        const actual = getPokerHand(combination);
+
+                        assert.equal(actual, `${combinationName}`);
+                    });
+                }
+            });
+        }
     });
 
-    // Напишите тесты на ваш замечательный код здесь
+    describe('Incorrect input', () => {
+        describe('Не массив', () => {
+            const inputs = ['lala', 5, undefined, null, () => {}, true, {}];
+            inputs.forEach(input => {
+                it(`should throw error for type ${typeof input}: ${input}`, () => {
+                    assert.throws(() => getPokerHand(input))
+                });
+            })
+        });
+
+        describe('Некорректный массив', () => {
+            const arrays = [
+                [],
+                [5],
+                [4, 2, 3, 4],
+                [6, 5, 3, 1, 2, 6]
+            ];
+            arrays.forEach(array => {
+                it(`should throw error for array: [${array}]`, () => {
+                    assert.throws(() => getPokerHand(array))
+                });
+            })
+        });
+    });
 });
