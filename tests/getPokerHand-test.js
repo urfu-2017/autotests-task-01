9c00ae196cc805a90f50c7a467037f11fe37b3e7
Maxim <32168435+Rolyx2@users.noreply.github.com>
2018-04-09 19:42:01 +0500

    Add files via upload

--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -7,6 +7,83 @@ describe('getPokerHand', () => {
 
         assert.equal(actual, 'Покер');
     });
+    it('should return `Каре` for [1, 6, 6, 6, 6]', () => {
+        const actual = getPokerHand([1, 6, 6, 6, 6]);
 
+        assert.equal(actual, 'Каре');
+    });
+    it('should return `Фулл хаус` for [4, 6, 4, 4, 6]', () => {
+        const actual = getPokerHand([4, 6, 4, 4, 6]);
+
+        assert.equal(actual, 'Фулл хаус');
+    });
+    it('should return `Тройка` for [4, 1, 4, 4, 6]', () => {
+        const actual = getPokerHand([4, 1, 4, 4, 6]);
+
+        assert.equal(actual, 'Тройка');
+    });
+    it('should return `Две пары` for [1, 6, 6, 4, 4]', () => {
+        const actual = getPokerHand([1, 6, 6, 4, 4]);
+
+        assert.equal(actual, 'Две Пары');
+    });
+    it('should return `Пара` for [1, 6, 3, 4, 6]', () => {
+        const actual = getPokerHand([1, 6, 3, 4, 6]);
+
+        assert.equal(actual, 'Пара');
+    });
+    it('should return `6` for [1, 2, 3, 4, 6]', () => {
+        const actual = getPokerHand([1, 2, 3, 4, 6]);
+
+        assert.equal(actual, '6');
+    });
     // Напишите тесты на ваш замечательный код здесь
+});
+
+describe('negative', () => {
+    it('Нужно ввести массив', () => {
+        try {
+            getPokerHand('Test');
+            throw new Error('`getPokerHand` should throw error')
+        } catch (error) {
+            assert.equal(error.message, 'You should enter Array');
+        }
+    });
+    it('Бросить нужно 5 костей, а не 3', () => {
+        try {
+            getPokerHand([1,2,3]);
+            throw new Error('`getPokerHand` should throw error')
+        } catch (error) {
+            assert.equal(error.message, 'Need throw 6 dices');
+        }
+    });
+    it('Бросить нужно не более 5', () => {
+        try {
+            getPokerHand([1,2,3,4,4,4,4]);
+            throw new Error('`getPokerHand` should throw error')
+        } catch (error) {
+            assert.equal(error.message, 'Need throw 6 dices');
+        }
+    });
+
+    });
+
+    describe('negative2', () => {    
+        it('Значения должны быть в промежутке от 1 до 6', () => {
+        try {
+            getPokerHand([1, 2, 3, 4, 7]);
+            throw new Error('`getPokerHand` should throw error')
+        } catch (error) {
+            assert.equal(error.message, 'Значения должны быть в промежутке от 1 до 6');
+        }
+    });
+    it('Значения должны быть целые', () => {
+        try {
+            getPokerHand([1.7, 2, 3, 5.4, 5.1]);
+            throw new Error('`getPokerHand` should throw error')
+        } catch (error) {
+            assert.equal(error.message, 'Значения должны быть целые');
+        }
+    });
+
 });
