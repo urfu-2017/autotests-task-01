6261c7f30e6e9da784e47a61246841e8126dc6d2
Maria <evchik.maria@gmail.com>
2018-04-09 23:01:23 +0500

    Реализовал определение комбинаций

--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -9,4 +9,40 @@ describe('getPokerHand', () => {
     });
 
     // Напишите тесты на ваш замечательный код здесь
+    
+    it('should return `Каре` for [1, 1, 1, 1, 2]', () => {
+        const actual = getPokerHand([1, 1, 1, 1, 2]);
+
+        assert.equal(actual, 'Каре');
+    });
+    
+    it('should return `Фулл хаус` for [1, 1, 1, 2, 2]', () => {
+        const actual = getPokerHand([1, 1, 1, 2, 2]);
+
+        assert.equal(actual, 'Фулл хаус');
+    });
+    
+    it('should return `Тройка` for [1, 1, 1, 2, 3]', () => {
+        const actual = getPokerHand([1, 1, 1, 2, 3]);
+
+        assert.equal(actual, 'Тройка');
+    });
+    
+    it('should return `Две пары` for [1, 1, 2, 2, 3]', () => {
+        const actual = getPokerHand([1, 1, 2, 2, 3]);
+
+        assert.equal(actual, 'Две пары');
+    });
+    
+    it('should return `Пара` for [1, 1, 2, 3, 4]', () => {
+        const actual = getPokerHand([1, 1, 2, 3, 4]);
+
+        assert.equal(actual, 'Пара');
+    });
+    
+    it('should return `Наивысшее очко` for [1, 2, 3, 4, 5]', () => {
+        const actual = getPokerHand([1, 2, 3, 4, 5]);
+
+        assert.equal(actual, 'Наивысшее очко');
+    });
 });
