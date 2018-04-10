044c1b03d7cf1801dafc158c4a1ba236e8cfe931
Anastasija Vasilenko <anastasija97vasilenko@yandex.ru>
2018-04-10 19:55:47 +0500

    Реализовал определение комбинаций

--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -7,6 +7,46 @@ describe('getPokerHand', () => {
 
         assert.equal(actual, 'Покер');
     });
+    it('should return `Каре` for [2, 1, 1, 1, 1]', () => {
+        const actual = getPokerHand([2, 1, 1, 1, 1]);
+
+        assert.equal(actual, 'Каре');
+    });
+    it('should return `Фулл хаус` for [1, 1, 3, 3, 1]', () => {
+        const actual = getPokerHand([1, 1, 3, 3 , 1]);
+
+        assert.equal(actual, 'Фулл хаус');
+    });
+    it('should return `Тройка` for [1, 1, 1, 4, 3]', () => {
+        const actual = getPokerHand([1, 1, 1, 1, 1]);
+
+        assert.equal(actual, 'Тройка');
+    });
+    it('should return `Две пары` for [1, 1, 5, 2, 2]', () => {
+        const actual = getPokerHand([1, 1, 1, 1, 1]);
+
+        assert.equal(actual, 'Две пары');
+    });
+    it('should return `Пара` for [1, 1, 2, 3, 4]', () => {
+        const actual = getPokerHand([1, 1, 2, 3, 4]);
+
+        assert.equal(actual, 'Пара');
+    });
+    it('should return `Наивысшее очко` for [1, 2, 4, 5, 3]', () => {
+        const actual = getPokerHand([1, 2, 4, 5, 3]);
+
+        assert.equal(actual, 'Наивысшее очко');
+    });
+    it('should return `exception` for [1, 6, 2, 4, 5, 3]', () => {
+        const actual = getPokerHand([1, 6, 2, 4, 5, 3]);
+
+        assert.equal(actual, 'exception');
+    });
+    it('should return `exception` for [7, 2, 4, 5, 3]', () => {
+        const actual = getPokerHand([7, 2, 4, 5, 3]);
+
+        assert.equal(actual, 'exception');
+    });
 
     // Напишите тесты на ваш замечательный код здесь
 });
