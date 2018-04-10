fc6ce2b81f89b03db3448bf9ac307c1074d85d36
d.fomin <d.fomin@skbkontur.ru>
2018-04-10 12:48:19 +0500

    all

--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -2,10 +2,21 @@ const assert = require('assert');
 const getPokerHand = require('../lib/getPokerHand');
 
 describe('getPokerHand', () => {
-    it('should return `Покер` for [1, 1, 1, 1, 1]', () => {
-        const actual = getPokerHand([1, 1, 1, 1, 1]);
-
-        assert.equal(actual, 'Покер');
+	var tests = [
+    {args: [1, 1, 1, 1, 1],       expected: `Покер`},
+    {args: [1, 1, 1, 1, 2],       expected: `Каре`},
+    {args: [1, 1, 1, 2, 2],       expected: `Фулл хаус`},
+    {args: [1, 1, 1, 2, 3],       expected: `Тройка`},
+    {args: [1, 1, 2, 2, 3],       expected: `Две пары`},
+    {args: [1, 1, 2, 4, 3],       expected: `Пара`},
+    {args: [1, 5, 2, 3, 4],       expected: `Наивысшее очко`}
+  ];
+  
+    tests.forEach(function(test) {
+        it('should return ' + test.expected + ' for ' + test.args.join(','), function() {
+          var res = getPokerHand(test.args);
+          assert.equal(res, test.expected);
+        });
     });
 
     // Напишите тесты на ваш замечательный код здесь
