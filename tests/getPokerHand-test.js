4635c3821f69ea0fc0e5b7e7a0dfc1cd6cda9c90
Anastasija Vasilenko <vasilenko.nastya@mail.ru>
2018-04-11 23:55:44 +0500

    Реализовал определение комбинаций

--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -37,25 +37,25 @@ describe('getPokerHand', () => {
 
         assert.equal(actual, 'Наивысшее очко');
     });
-    it('should return `Длина больше требуемой` for [1, 6, 2, 4, 5, 3]', () => {
+    it('should throw new Error (\'Длина больше требуемой\') for [1, 6, 2, 4, 5, 3]', () => {
         const actual = getPokerHand([1, 6, 2, 4, 5, 3]);
 
-        assert.equal(actual, 'Длина больше требуемой');
+        assert.equal(error.message, ('Длина больше требуемой'));
     });
-    it('should return `Длина меньше требуемой` for [2, 4, 5, 3]', () => {
+    it('should throw new Error (\'Длина меньше требуемой\') for [2, 4, 5, 3]', () => {
         const actual = getPokerHand([ 2, 4, 5, 3]);
 
-        assert.equal(actual, 'Длина меньше требуемой');
+        assert.equal(error.message, ('Длина меньше требуемой'));
     });
-    it('should return `Введенных значений нет на игральной кости` for [7, 2, 4, 5, 3]', () => {
+    it('should throw new Error (\'Введенных значений нет на игральной кости\') for [7, 2, 4, 5, 3]', () => {
         const actual = getPokerHand([7, 2, 4, 5, 3]);
 
-        assert.equal(actual, 'Введенных значений нет на игральной кости');
+        assert.equal(error.message, ('Введенных значений нет на игральной кости'));
     });
-    it('should return `Введенных значений нет на игральной кости` for [\'a\', \'g\', 4, 5, 3]', () => {
+    it('should throw new Error (\'Введенных значений нет на игральной кости\') for [\'a\', \'g\', 4, 5, 3]', () => {
         const actual = getPokerHand(['a', 'g', 4, 5, 3]);
 
-        assert.equal(actual, 'Введенных значений нет на игральной кости');
+        assert.equal(error.message, ('Введенных значений нет на игральной кости'));
     });
 
     // Напишите тесты на ваш замечательный код здесь
