6b63ea180f3cbc7e9471a3d52058d1fd71522ae8
AnnaIlinykh <privet587@mail.ru>
2018-04-11 19:12:21 +0500

    Написала функцию

--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -8,5 +8,64 @@ describe('getPokerHand', () => {
         assert.equal(actual, 'Покер');
     });
 
+    it('should return `Каре` for [1, 1, 1, 1, 2]', () => {
+        const actual = getPokerHand([1, 1, 1, 1, 2]);
+
+        assert.equal(actual, 'Каре');
+    });
+
+
+    it('should return `Фулл хаус` for [1, 1, 1, 2, 2]', () => {
+        const actual = getPokerHand([1, 1, 1, 2, 2]);
+
+        assert.equal(actual, 'Фулл хаус');
+    });
+
+    it('should return `Тройка` for [1, 1, 1, 2, 3]', () => {
+        const actual = getPokerHand([1, 1, 1, 2, 3]);
+
+        assert.equal(actual, 'Тройка');
+    });
+	
+	it('should return `Две пары` for [1, 1, 2, 2, 3]', () => {
+        const actual = getPokerHand([1, 1, 2, 2, 3]);
+
+        assert.equal(actual, 'Две пары');
+    });
+   
+    it('should return `Пара` for [1, 1, 2, 3, 4]', () => {
+        const actual = getPokerHand([1, 1, 2, 3, 4]);
+
+        assert.equal(actual, 'Пара');
+    });
+	
+	it('should return `Наивысшее очко` for [1, 2, 3, 4, 5]', () => {
+        const actual = getPokerHand([1, 2, 3, 4, 5]);
+
+        assert.equal(actual, 'Наивысшее очко');
+    });
+
+
+	it('should throw error when there is less then 5 dice', () => {
+		 const actual =  getPokerHand([1, 2, 4, 5]);
+
+		 assert.equal(actual, 'Неверное число костей');
+	});
+
+
+	it('should throw error when there is more then 5 dice', () => {
+		 const actual =  getPokerHand([1, 2, 4, 5, 6, 6]);
+
+		 assert.equal(actual, 'Неверное число костей');
+	});
+    
+    it('should throw error when there is wrong dices', () => {
+		 const actual = () => getPokerHand([1, 2, 4, 5, 'word']);
+
+		 assert.throws(actual, /Значения должны быть числами/);
+	});
+
+	
+
     // Напишите тесты на ваш замечательный код здесь
 });
