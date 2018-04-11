faa45cdde5b99cc7776d75e795a7266c4be8f085
chrighter <christina.ilyenko@yandex.ru>
2018-04-11 15:07:57 +0500

    first version

--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -9,4 +9,111 @@ describe('getPokerHand', () => {
     });
 
     // Напишите тесты на ваш замечательный код здесь
+    it('should return `Каре` for [1, 1, 1, 1, 2]', () => {
+        const actual = getPokerHand([1, 1, 1, 1, 2]);
+
+        assert.equal(actual, 'Каре');
+    });
+
+    it('should return `Каре` for [2, 1, 1, 1, 1]', () => {
+        const actual = getPokerHand([2, 1, 1, 1, 1]);
+
+        assert.equal(actual, 'Каре');
+    });
+
+    it('should return `Фулл хаус` for [1, 1, 1, 2, 2]', () => {
+        const actual = getPokerHand([1, 1, 1, 2, 2]);
+
+        assert.equal(actual, 'Фулл хаус');
+    });
+
+    it('should return `Фулл хаус` for [1, 2, 1, 2, 1]', () => {
+        const actual = getPokerHand([1, 2, 1, 2, 1]);
+
+        assert.equal(actual, 'Фулл хаус');
+    });
+
+    it('should return `Тройка` for [1, 1, 1, 2, 3]', () => {
+        const actual = getPokerHand([1, 1, 1, 2, 3]);
+
+        assert.equal(actual, 'Тройка');
+    });
+
+    it('should return `Тройка` for [1, 3, 4, 1, 1]', () => {
+        const actual = getPokerHand([1, 3, 4, 1, 1]);
+
+        assert.equal(actual, 'Тройка');
+    });
+
+    it('should return `Две пары` for [1, 1, 2, 2, 3]', () => {
+        const actual = getPokerHand([1, 1, 2, 2, 3]);
+
+        assert.equal(actual, 'Две пары');
+    });
+
+    it('should return `Две пары` for [3, 1, 2, 1, 3]', () => {
+        const actual = getPokerHand([3, 1, 2, 1, 3]);
+
+        assert.equal(actual, 'Две пары');
+    });
+
+    it('should return `Пара` for [1, 1, 2, 3, 4]', () => {
+        const actual = getPokerHand([1, 1, 2, 3, 4]);
+
+        assert.equal(actual, 'Пара');
+    });
+
+    it('should return `Пара` for [5, 5, 2, 3, 4]', () => {
+        const actual = getPokerHand([5, 5, 2, 3, 4]);
+
+        assert.equal(actual, 'Пара');
+    });
+
+    it('should return `Наивысшее очко` for [1, 2, 3, 4, 5]', () => {
+        const actual = getPokerHand([1, 2, 3, 4, 5]);
+
+        assert.equal(actual, 'Наивысшее очко');
+    });
+
+    it('should return `Некорректный ввод` for lalala', () => {
+        const actual = getPokerHand('lalala');
+
+        assert.equal(actual, 'Некорректный ввод');
+    });
+
+    it('should return `Некорректный ввод` for [1, 2, 3, 4]', () => {
+        const actual = getPokerHand([1, 2, 3, 4]);
+
+        assert.equal(actual, 'Некорректный ввод');
+    });
+
+    it('should return `Некорректный ввод` for []', () => {
+        const actual = getPokerHand([]);
+
+        assert.equal(actual, 'Некорректный ввод');
+    });
+
+    it('should return `Некорректный ввод` for [1, 1, 1, 1, 1, 1]', () => {
+        const actual = getPokerHand([]);
+
+        assert.equal(actual, 'Некорректный ввод');
+    });
+
+    it('should return `Некорректный ввод` for [99, 1, 1, 1, 1]', () => {
+        const actual = getPokerHand([]);
+
+        assert.equal(actual, 'Некорректный ввод');
+    });
+
+    it('should return `Некорректный ввод` for [0, 1, 1, 1, 1]', () => {
+        const actual = getPokerHand([]);
+
+        assert.equal(actual, 'Некорректный ввод');
+    });
+
+    it('should return `Некорректный ввод` for [0, a, b, c, 1]', () => {
+        const actual = getPokerHand([]);
+
+        assert.equal(actual, 'Некорректный ввод');
+    });
 });
