5c72357b7e658d9afe53eb9a5d4022654c106c99
Andreyproger <crashc@yandex.ru>
2018-04-16 21:22:38 +0500

    Комулов Андрей

--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -10,47 +10,35 @@ describe('getPokerHand', () => {
 
 	        assert.equal(actual, 'Покер');
 	    });
-	    
+
+	    it('should return `Каре` for [1, 1, 1, 1, 2]', () =>{
+	    	const actual = getPokerHand([1, 1, 1, 1, 2]);
+
+	    	assert.equal(actual, 'Каре');
+	    });
+
 	    it('should return `Фулл Хаус` for [1, 1, 1, 3, 3]', () =>{
 	    	const actual = getPokerHand([1, 1, 1, 3, 3]);
 
 	    	assert.equal(actual, `Фулл Хаус`);
 	    });
 
-	    it('should return `Пара` for [1, 2, 2, 3, 4]', () =>{
-	    	const actual = getPokerHand([1, 2, 2, 3, 4]);
-
-	    	assert.equal(actual, `Пара`);
-	    });
-
-	    it('should return `Пара` for [1, 2, 3, 4, 4]', () =>{
-	    	const actual = getPokerHand([1, 2, 3, 4, 4]);
-
-	    	assert.equal(actual, `Пара`);
-	    });
-
-	    it('should return `Пара` for [1, 1, 2, 3, 4]', () =>{
-	    	const actual = getPokerHand([1, 1, 2, 3, 4]);
-
-	    	assert.equal(actual, `Пара`);
-	    });
-
-	    it('should return `Пара` for [1, 2, 2, 3, 4]', () =>{
-	    	const actual = getPokerHand([1, 2, 2, 3, 4]);
-
-	    	assert.equal(actual, `Пара`);
-	    });
-
 	    it('should return `Тройка` for [1, 1, 1, 4, 3]', () =>{
 	    	const actual = getPokerHand([1, 1, 1, 4, 3]);
 
-	    	assert.equal(actual, `Пара`);
+	    	assert.equal(actual, `Тройка`);
 	    });
 
 	    it('should return `Две пары` for [1, 2, 2, 4, 4]', () =>{
 	    	const actual = getPokerHand([1, 2, 2, 4, 4]);
 
 	    	assert.equal(actual, `Две пары`);
+	    });
+
+		it('should return `Пара` for [1, 2, 2, 3, 4]', () =>{
+	    	const actual = getPokerHand([1, 2, 2, 3, 4]);
+
+	    	assert.equal(actual, `Пара`);
 	    });
 
 	    it('should retrn `Наивысшее очко` for [1, 3, 4, 5, 2]',() => {
@@ -61,29 +49,54 @@ describe('getPokerHand', () => {
 	});
 
 	describe('Not Positive', () =>{
-		it('should return `It is not array` for []', () => {
+
+		it('should return `Empty input` for ' '', () => {
+			const actual = () => getPokerHand();
+
+			assert.equal(actual, /Empty input/);
+		}); //nothing input
+
+		it('should return `Empty array` for []', () => {
 	        const actual = () => getPokerHand([]);
 
-	        assert.equal(actual, /Its not array/);
-	    });
+	        assert.equal(actual, /Empty array/);
+	    }); // on input empty array
 
-	    it('should return `It is not array` for [1]', () => {
-	        const actual = () => getPokerHand([]);
+	    it('should return `In array should be more than 5 elements` for [1,2,3,4,5,6]', () => {
+	        const actual = () => getPokerHand([1,2,3,4,5,6]);
 
-	        assert.equal(actual, /Need five elements/);
-	    });
+	        assert.equal(actual, /In array should be more than 5 elements/);
+	    }); // more than 5 elements
 
-	    it('should return `It is not array` for [1]', () => {
+	    it('should return `In array should be more than 5 elements` for [1,2,3,4,5,6]', () => {
+	        const actual = () => getPokerHand([1,2,3,4]);
+
+	        assert.equal(actual, /In array should be less than 5 elements/);
+	    }); // less than 5 elements
+
+		it('should return `It is not array` for NaN', () => {
 	        const actual = () => getPokerHand(NaN);
 
-	        assert.equal(actual, /Its not array/);
+	        assert.equal(actual, /Its not number/);
+	    }); // input not array
+		
+	    it('should return `Cell of array less 1` for [0,2,3,4,5]', () => {
+	    	const actual = () => getPokerHand([0,2,3,4,5]);
+
+	    	assert.equal(actual, /Cell of array less 1/);
 	    });
 
-	    it('should return `It is not array` for [1,2,3,4,5,6]', () => {
-	        const actual = () => getPokerHand(NaN);
+		it('should return `Cell of array less 6` for [1,7,3,4,5]', () => {
+	    	const actual = () => getPokerHand([0,2,3,4,5]);
 
-	        assert.equal(actual, /In array should be 5 elements/);
-	    });
+	    	assert.equal(actual, /Cell of array less 6/);
+	    });	     
+
+		it('should return `Element is `Cell of array is fraction` for [0.8, 2, 3, 4, 5]', () =>{
+			const actual = () => getPokerHand([0.8, 2, 3, 4, 5]);
+
+			assert.equal(actual, /Cell of array is fraction/);
+		});  
 	});
     // Напишите тесты на ваш замечательный код здесь
 });
