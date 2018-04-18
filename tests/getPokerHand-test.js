6e4204887e619956bcd912fb7ee306c6ca6c3410
romassol <romass1998@mail.ru>
2018-04-18 20:16:40 +0500

    resolved some problems

--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -1,70 +1,36 @@
 const assert = require('assert');
 const getPokerHand = require('../lib/getPokerHand');
 
+function getCombinationTest(name, dice) {
+    return it(`should return '${name}' for [${dice}]`, () => {
+        const actual = getPokerHand(dice);
+        assert.equal(actual, name);
+    });
+}
+
+function getErrorTest(testName, dice) {
+    return it(`should throw error ${testName}`, () => {
+        assert.throws(() => getPokerHand(dice),
+            'Dice should contains only 5 integer numbers from 1 to 6');
+    });
+}
+
 describe('getPokerHand', () => {
-    it('should return `Покер` for [1, 1, 1, 1, 1]', () => {
-        const actual = getPokerHand([1, 1, 1, 1, 1]);
+    getCombinationTest('Покер', [1, 1, 1, 1, 1]);
+    getCombinationTest('Каре',[3, 1, 3, 3, 3]);
+    getCombinationTest('Фулл хаус', [5, 2, 5, 5, 2]);
+    getCombinationTest('Тройка', [4, 6, 1, 6, 6]);
+    getCombinationTest('Две пары', [2, 4, 4, 1, 2]);
+    getCombinationTest('Пара', [6, 4, 4, 1, 2]);
+    getCombinationTest('Наивысшее очко', [6, 4, 3, 1, 2]);
 
-        assert.equal(actual, 'Покер');
-    });
 
-    it('should return `Каре` for [3, 1, 3, 3, 3]', () => {
-        const actual = getPokerHand([3, 1, 3, 3, 3]);
-
-        assert.equal(actual, 'Каре');
-    });
-
-    it('should return `Фулл хаус` for [5, 2, 5, 5, 2]', () => {
-        const actual = getPokerHand([5, 2, 5, 5, 2]);
-
-        assert.equal(actual, 'Фулл хаус');
-    });
-
-    it('should return `Тройка` for [4, 6, 1, 6, 6]', () => {
-        const actual = getPokerHand([4, 6, 1, 6, 6]);
-
-        assert.equal(actual, 'Тройка');
-    });
-
-    it('should return `Две пары` for [2, 4, 4, 1, 2]', () => {
-        const actual = getPokerHand([2, 4, 4, 1, 2]);
-
-        assert.equal(actual, 'Две пары');
-    });
-
-    it('should return `Пара` for [6, 4, 4, 1, 2]', () => {
-        const actual = getPokerHand([6, 4, 4, 1, 2]);
-
-        assert.equal(actual, 'Пара');
-    });
-
-    it('should throw error on values more 6', () => {
-        assert.throws(() => getPokerHand([6, 7, 4, 1, 2]),
-            'Dice should contains only numbers from 1 to 6');
-    });
-
-    it('should throw error on values less 1', () => {
-        assert.throws(() => getPokerHand([6, 3, 4, 1, -5]),
-            'Dice should contains only numbers from 1 to 6');
-    });
-
-    it('should throw error on not integer values', () => {
-        assert.throws(() => getPokerHand([6, 3, 'hey', 1, 3]),
-            'Dice should contains only numbers from 1 to 6');
-    });
-
-    it('should throw error when count of dice throw more 5', () => {
-        assert.throws(() => getPokerHand([6, 3, 1, 1, 3, 4]),
-            'Count of dice throw should equals 5');
-    });
-
-    it('should throw error when count of dice throw less 5', () => {
-        assert.throws(() => getPokerHand([6, 3, 1, 1]),
-            'Count of dice throw should equals 5');
-    });
-
-    it('should throw error when input is not an array', () => {
-        assert.throws(() => getPokerHand('hey'),
-            'Input should be an array');
-    });
+    getErrorTest('on values more 6', [6, 7, 4, 1, 2]);
+    getErrorTest('on values less 1', [6, 3, 4, 1, -5]);
+    getErrorTest('on array with string values', [6, 3, 'hey', 1, 3]);
+    getErrorTest('on array with float values', [6, 3, 1, 1, 1.9]);
+    getErrorTest('when count of dice throw more 5', [6, 3, 1, 1, 3, 4]);
+    getErrorTest('when count of dice throw less 5', [6, 3, 1, 1]);
+    getErrorTest('when input is a string', 'hey');
+    getErrorTest('when input is empty');
 });
