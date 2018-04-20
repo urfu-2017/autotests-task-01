2d4426a663090ab74faba570252881c03b7fb286
dimastark <dimastark@yandex-team.ru>
2018-04-20 17:00:41 +0500

    Поправил все замечания, вынес сообщения об ошибках в конфиг

--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -3,50 +3,43 @@
 const assert = require('assert');
 const getPokerHand = require('../lib/getPokerHand');
 
-const names = require('../lib/combinations').names;
+const config = require('../lib/config');
 
 describe('getPokerHand', () => {
-    const testCases = [
-        { arg: [1, 1, 1, 1, 1], expected: names.poker },
-        { arg: [2, 2, 2, 2, 2], expected: names.poker },
-        { arg: [3, 3, 3, 3, 3], expected: names.poker },
-        { arg: [1, 1, 1, 1, 2], expected: names.four },
-        { arg: [1, 1, 2, 1, 1], expected: names.four },
-        { arg: [3, 3, 5, 3, 3], expected: names.four },
-        { arg: [1, 1, 1, 2, 2], expected: names.fullHouse },
-        { arg: [2, 1, 1, 1, 2], expected: names.fullHouse },
-        { arg: [4, 1, 4, 1, 1], expected: names.fullHouse },
-        { arg: [1, 1, 1, 2, 3], expected: names.triple },
-        { arg: [1, 2, 1, 1, 3], expected: names.triple },
-        { arg: [3, 1, 3, 3, 5], expected: names.triple },
-        { arg: [1, 1, 2, 2, 3], expected: names.pairs },
-        { arg: [1, 2, 1, 2, 3], expected: names.pairs },
-        { arg: [3, 3, 5, 2, 5], expected: names.pairs },
-        { arg: [1, 1, 2, 3, 4], expected: names.pair },
-        { arg: [2, 3, 4, 5, 5], expected: names.pair },
-        { arg: [2, 1, 5, 3, 2], expected: names.pair },
-        { arg: [1, 2, 3, 4, 5], expected: names.best },
-        { arg: [5, 4, 3, 2, 1], expected: names.best },
-        { arg: [1, 3, 2, 5, 4], expected: names.best },
-        { arg: [1, 2, 3, 4], expected: 'error' },
-        { arg: [1, 2, 3, 4, 1.2], expected: 'error' },
-        { arg: [1, 2, 3, 4, 'a'], expected: 'error' },
-        { arg: [1, 2, 3, 4, 5, 1], expected: 'error' },
-        { arg: [1, 2, 3, 4, 6], expected: 'error' },
-        { arg: [1, 2, 3, 4, -1], expected: 'error' },
-        { arg: undefined, expected: 'error' },
-        { arg: null, expected: 'error' },
-        { arg: 1234, expected: 'error' },
-        { arg: [], expected: 'error' }
-    ];
+    [
+        { dices: [1, 1, 1, 1, 1], expected: config.names.poker },
+        { dices: [6, 6, 6, 6, 6], expected: config.names.poker },
+        { dices: [1, 1, 1, 1, 2], expected: config.names.four },
+        { dices: [1, 1, 2, 1, 1], expected: config.names.four },
+        { dices: [1, 1, 1, 2, 2], expected: config.names.fullHouse },
+        { dices: [1, 2, 1, 1, 2], expected: config.names.fullHouse },
+        { dices: [1, 1, 1, 2, 3], expected: config.names.triple },
+        { dices: [1, 2, 1, 1, 3], expected: config.names.triple },
+        { dices: [1, 1, 2, 2, 3], expected: config.names.pairs },
+        { dices: [1, 2, 1, 2, 3], expected: config.names.pairs },
+        { dices: [1, 1, 2, 3, 4], expected: config.names.pair },
+        { dices: [1, 2, 1, 3, 4], expected: config.names.pair },
+        { dices: [1, 2, 3, 4, 5], expected: config.names.best }
+    ].forEach(testCase =>
+        it(`should return '${testCase.expected}' for ${JSON.stringify(testCase.dices)}`, () =>
+            assert.equal(getPokerHand(testCase.dices), testCase.expected)
+        )
+    );
 
-    testCases.forEach(testCase => {
-        it(`should return '${testCase.expected}' for ${testCase.arg}`, () => {
-            if (testCase.expected === 'error') {
-                assert.throws(() => getPokerHand(testCase.arg));
-            } else {
-                assert.equal(getPokerHand(testCase.arg), testCase.expected);
-            }
-        });
-    });
+    [
+        { dices: [], expectedError: config.errors.invalidCount },
+        { dices: [1, 2, 3, 4], expectedError: config.errors.invalidCount },
+        { dices: [1, 2, 3, 4, 5, 1], expectedError: config.errors.invalidCount },
+        { dices: [1, 2, 3, 4, 7], expectedError: config.errors.invalidValues },
+        { dices: [1, 2, 3, 4, -1], expectedError: config.errors.invalidValues },
+        { dices: [1, 2, 3, 4, 1.2], expectedError: config.errors.invalidValues },
+        { dices: [1, 2, 3, 4, 'a'], expectedError: config.errors.invalidValues },
+        { dices: null, expectedError: config.errors.notArray },
+        { dices: 1234, expectedError: config.errors.notArray },
+        { dices: undefined, expectedError: config.errors.notArray }
+    ].forEach(testCase =>
+        it(`should raise '${testCase.expectedError}' for ${JSON.stringify(testCase.dices)}`, () =>
+            assert.throws(() => getPokerHand(testCase.dices), new RegExp(testCase.expectedError))
+        )
+    );
 });
