2005c52fa6f0d80c7eb3b3f37be7ae4637367504
Andrey <crashc@yandex.ru>
2018-04-26 11:15:37 +0500

    Update getPokerHand-test.js

--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -1,102 +1,42 @@
-'use strict';
-
 const assert = require('assert');
 const getPokerHand = require('../lib/getPokerHand');
 
 describe('getPokerHand', () => {
-	describe('Positive', () =>{
-	    it('should return `Покер` for [1, 1, 1, 1, 1]', () => {
-	        const actual = getPokerHand([1, 1, 1, 1, 1]);
-
-	        assert.equal(actual, 'Покер');
-	    });
-
-	    it('should return `Каре` for [1, 1, 1, 1, 2]', () =>{
-	    	const actual = getPokerHand([1, 1, 1, 1, 2]);
-
-	    	assert.equal(actual, 'Каре');
-	    });
-
-	    it('should return `Фулл Хаус` for [1, 1, 1, 3, 3]', () =>{
-	    	const actual = getPokerHand([1, 1, 1, 3, 3]);
-
-	    	assert.equal(actual, `Фулл Хаус`);
-	    });
-
-	    it('should return `Тройка` for [1, 1, 1, 4, 3]', () =>{
-	    	const actual = getPokerHand([1, 1, 1, 4, 3]);
-
-	    	assert.equal(actual, `Тройка`);
-	    });
-
-	    it('should return `Две пары` for [1, 2, 2, 4, 4]', () =>{
-	    	const actual = getPokerHand([1, 2, 2, 4, 4]);
-
-	    	assert.equal(actual, `Две пары`);
-	    });
-
-		it('should return `Пара` for [1, 2, 2, 3, 4]', () =>{
-	    	const actual = getPokerHand([1, 2, 2, 3, 4]);
-
-	    	assert.equal(actual, `Пара`);
-	    });
-
-	    it('should retrn `Наивысшее очко` for [1, 3, 4, 5, 2]',() => {
-            const actual = getPokerHand([1, 3, 4, 5, 2]);
-
-            assert.equal(actual, 'Наивысшее очко');
+	var tests = [
+    {args: [1, 1, 1, 1, 1],       expected: `Покер`},
+    {args: [1, 1, 1, 1, 2],       expected: `Каре`},
+    {args: [1, 1, 1, 2, 2],       expected: `Фулл хаус`},
+    {args: [1, 1, 1, 2, 3],       expected: `Тройка`},
+    {args: [1, 1, 2, 2, 3],       expected: `Две пары`},
+    {args: [1, 1, 2, 4, 3],       expected: `Пара`},
+    {args: [1, 5, 2, 3, 4],       expected: `Наивысшее очко`}
+  ];
+  
+    tests.forEach(function(test) {
+        it('should return ' + test.expected + ' for ' + test.args.join(','), function() {
+          var res = getPokerHand(test.args);
+          assert.equal(res, test.expected);
         });
-	});
+    });
+	
+	it('should throw for more than 5 cards', function() {
+	    var res = () => getPokerHand([1, 2, 3, 4, 5, 6]);
+	    assert.throws(res, Error);
+    });
+	it('should not throw on points greater than 5', function() {
+	    var res = () => getPokerHand([6, 6, 6, 6 ,6]);
+	    assert.doesNotThrow(res, Error);
+    });
+	
+	it('should throw on negative points', function() {
+	    var res = () => getPokerHand([-1, -1, -1, -1 ,-1]);
+	    assert.throws(res, Error);
+    });
+	
+	it('should throw on incorrect input', function() {
+	    var res = () => getPokerHand('a');
+	    assert.throws(res, Error);
+    });
 
-	describe('Not Positive', () =>{
-
-		it('should return `Empty input` for empty input', () => {
-			const actual = () => getPokerHand();
-
-			assert.equal(actual, `Empty input`);
-		}); //nothing input
-
-		it('should return `Empty array` for []', () => {
-	        const actual = () => getPokerHand([]);
-
-	        assert.equal(actual, `Empty array`);
-	    }); // on input empty array
-
-	    it('should return `In array should be more than 5 elements` for [1,2,3,4,5,6]', () => {
-	        const actual = () => getPokerHand([1,2,3,4,5,6]);
-
-	        assert.equal(actual, `In array should be more than 5 elements`);
-	    }); // more than 5 elements
-
-	    it('should return `In array should be more than 5 elements` for [1,2,3,4]', () => {
-	        const actual = () => getPokerHand([1,2,3,4]);
-
-	        assert.equal(actual, `In array should be less than 5 elements`);
-	    }); // less than 5 elements
-
-		it('should return `It is not array for NaN`', () => {
-	        const actual = () => getPokerHand(NaN);
-
-	        assert.equal(actual, `It is not array for NaN`);
-	    }); // input not array
-		
-	    it('should return `Cell of array less 1 for [0,2,3,4,5]`', () => {
-	    	const actual = () => getPokerHand([0,2,3,4,5]);
-
-	    	assert.equal(actual, `Cell of array less 1 for [0,2,3,4,5]`);
-	    });
-
-		it('should return `Cell of array less 6` for [1,7,3,4,5]', () => {
-	    	const actual = () => getPokerHand([1,7,3,4,5]);
-
-	    	assert.equal(actual, `Cell of array less 6`);
-	    });	     
-
-		it('should return `Cell of array is fraction for [0.8, 2, 3, 4, 5]`', () =>{
-			const actual = () => getPokerHand([0.8, 2, 3, 4, 5]);
-
-			assert.equal(actual, `Cell of array is fraction for [0.8, 2, 3, 4, 5]`);
-		});  
-	});
     // Напишите тесты на ваш замечательный код здесь
 });
