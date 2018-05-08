5059f05fb7ea49252132491fa9fa43878deab5f6
Мишакова Наталья Сергеевна <mishakova.n@mail.ru>
2018-05-08 10:45:50 +0500

    Update getPokerHand-test.js

--- tests/getPokerHand-test.js
+++ tests/getPokerHand-test.js
@@ -24,13 +24,13 @@ describe('getPokerHand', () => {
         const actual = getPokerHand([6, 4, 2, 1, 5]); assert.equal(actual, 'Наивысшее очко');
     });
 	it('should throw error when length is not 5', () => {
-     const actual = () => getPokerHand([1, 1, 1, 1]); assert.throws(actual, /This is not a combination for poker/);
+     const actual = () => getPokerHand([1, 1, 1, 1]); assert.throws(actual, /This is not a set of five elements/);
 	 });
 	 it('should throw error when length is not 5', () => {
-     const actual = () => getPokerHand([1, 2, 3, 4, 5, 6]); assert.throws(actual, /This is not a combination for poker/);
+     const actual = () => getPokerHand([1, 2, 3, 4, 5, 6]); assert.throws(actual, /This is not a set of five elements/);
 	 });
-	 it('should throw error when combination is not a Number', () => {
-     const actual = () => getPokerHand([1, 1, 'h', 1,5]); assert.throws(actual, /There is not a Number/);
+	 it('should throw error when the combination contains not a number', () => {
+     const actual = () => getPokerHand([1, 1, 'h', 1,5]); assert.throws(actual, /Оne or more elements are not a number/);
 	 });
 	 it('should throw error when value not for the cube', () => {
      const actual = () => getPokerHand([1, 8, 1, 3, 1,]); assert.throws(actual, /There is a value not for the cube/);
